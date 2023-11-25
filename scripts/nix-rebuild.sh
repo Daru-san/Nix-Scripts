f5b8dbfb2c114da250e348e86d7a31b5f4291675
Daru-san <mica.tui@proton.me>
2023-11-25 08:01:01 +0200

    feat: update script big time

--- scripts/nix-rebuild.sh
+++ scripts/nix-rebuild.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 #Create an option to show help
 help() {
-	printf "Options:"
-	printf "\n -m switch/boot - specifiy whether you want to switch to config now or at next boot, default is switch\n"
+	printf "This is a custom nix-rebuild script made for flake users\n"
+	printf "\nOptions:"
+	printf "\n-m switch/boot :      specifiy whether you want to switch to config now or at next boot, default is switch\n"
+	printf "\n-r repo name :        specify where the repo to build from is e.g ~/.dotfiles\n"
+	printf "\n"
 }
 
-while getopts h:m: flag; do
+while getopts r:h:m: flag; do
 	case "${flag}" in
 	m) mode=${OPTARG} ;;
+	r) repo=${OPTARG} ;;
 	h)
-		help
+		help | less
 		exit
 		;;
 	esac
 done
+
 if [ mode != 'switch' ] && [ mode != 'boot' ]; then
 	mode='switch'
 fi
 
 hostname=$(hostname -f)
-cd $HOME/Projects/repo
+cd $repo
 sudo nixos-rebuild ${mode} --flake .#${hostname} --impure
