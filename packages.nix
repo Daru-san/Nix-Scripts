d8cc3058d105fcde4c611dd8e0d79124488e359f
Daru-san <mica.tui@proton.me>
2024-01-06 17:31:56 +0200

    fix incorrect package name

--- packages.nix
+++ packages.nix
@@ -2,7 +2,7 @@
 pkgs: 
 {
   hm-build = pkgs.callPackage ./nix/hm-build.nix { };
-  colorpicker = pkgs.callPackage ./nix/color-picker.nix { };
+  color-picker = pkgs.callPackage ./nix/color-picker.nix { };
   nix-rebuild = pkgs.callPackage ./nix/nix-rebuild.nix { };
   default = pkgs.callPackage ./nix/default.nix;
 }
